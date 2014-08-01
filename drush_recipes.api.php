10acfa0fbf8119625af09a5e1f722c49a955a0a7
btopro <bto108@psu.edu>
2014-07-31 22:49:32 -0400

    4 call structures documented

--- drush_recipes.api.php
+++ drush_recipes.api.php
@@ -17,7 +17,7 @@
  *   dictate this though in the future they will be enabled ahead of time.
  * recipe - the structure of commands to execute, this can also be another
  *   recipe filename which will append all the commands in that file ahead of
- *   what is about to execute.
+ *   what is about to execute. There are 4 structures to this listed below
  * metadata - a series of properties that can be used for front-end integration.
  *   this is entirely optional but helps developers understand what you wrote.
  */
@@ -63,3 +63,28 @@ $js = <<<JS
   }
 }
 JS;
+// here's the 4 major types of calls you can do in a recipe as of the 1.0 spec.
+"recipe": [
+    "dr_admin_update_status.drecipe", // reference another recipe
+    [
+      "vset", // each element is an argument passed into a 1 line call w/ drush as prefix
+      "user_register",
+      "1"
+    ],
+    [
+      "conditional": [ // allow for user to select which sub-routine to run
+        "recipe1.drecipe",
+        "recipe2.drecipe",
+        "recipe3.drecipe"
+      ]
+    ],
+    [// long call format, this allows user to interact w/ the call
+    // only use this for complex call structures that you need to interact with
+    "target": "cool.d7.site", //target
+    "arguments": [ // arguments to pass in, similar to simple structure
+      "en",
+      "paranoia"
+    ],
+    "options": ["y"] // any possible options
+    ]
+  ],
